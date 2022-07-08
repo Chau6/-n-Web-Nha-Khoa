10d38a7dffa9f4b3c57a957b9b1e1b79785b06da
LODNight <khuunhattin123@gmail.com>
2022-07-08 15:45:47 +0700

    có cái nịt

--- database/migrations/2022_07_08_084423_create_category_table.php
+++ database/migrations/2022_07_08_084423_create_category_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('category');
     }
 };
