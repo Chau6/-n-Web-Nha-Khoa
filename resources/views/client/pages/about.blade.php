d6174816843968e695f67ac85a3320d285e85fa9
Chau6 <tanchau0602@gmail.com>
2022-07-10 01:49:52 +0700

    Test

--- resources/views/client/pages/about.blade.php
+++ resources/views/client/pages/about.blade.php
@@ -11,7 +11,7 @@
             <div class="col-xl-12">
                 <div class="inner-content clearfix">
                     <div class="title float-left">
-                       <h2>About Us</h2>
+                       <h2>Hello About</h2>
                     </div>
                     <div class="breadcrumb-menu float-right">
                         <ul class="clearfix">
