a3efb0f1379d3c10136f2b3cc7b0a70ee2df709e
LODNight <khuunhattin123@gmail.com>
2022-07-13 13:39:02 +0700

    indexHome

--- app/Http/Controllers/Admin/IndexPageController.php
+++ app/Http/Controllers/Admin/IndexPageController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IndexPageController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $count_user = DB::table('user')->selectRaw("count(case when level = '0' then 1 end) as user")->first();
+        $count_admin = DB::table('user')->selectRaw("count(case when level = '1' then 1 end) as admin")->first();
+        return view('admin.index',['total_user'=>$count_user, 'total_admin'=>$count_admin]);
     }
 }
